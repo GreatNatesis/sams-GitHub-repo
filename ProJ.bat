aae1fd9578984bf849187744c291c9b0564cd17f
Nate <99703235+GreatNate0509YT@users.noreply.github.com>
2022-11-05 21:11:46 -0500

    faulty

--- ProJ.bat
+++ ProJ.bat
@@ -1,1 +1,23 @@
+@echo off
+color 0a
+title center command
+:start
+cls
+echo cat (shows you a cat photo) 
 
+
+
+
+
+
+set /p main= enter_command:
+goto %main%
+
+
+
+:cat
+cls
+start https://i.guim.co.uk/img/media/c5e73ed8e8325d7e79babf8f1ebbd9adc0d95409/2_5_1754_1053/master/1754.jpg?width=620&quality=85&dpr=1&s=none
+pause
+cls
+goto start 
