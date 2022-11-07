bb828903d71ddec59ff199c3ea7866c0e6362481
GreatNate0509YTALT1 <115590391+GreatNate0509YTALT1@users.noreply.github.com>
2022-11-06 20:10:27 -0600

    Update Project.bat

--- Project.bat
+++ Project.bat
@@ -1,9 +1,9 @@
-
 @echo off
 title my proj
 :start
 cls
 echo cat [gives you a cat]
+echo hello [hello]
 
 
 
@@ -23,6 +23,22 @@ goto %main%
 :cat
 cls
 start https://wallpapers.com/wallpapers/cool-profile-pictures-hoodie-cat-6dkl56hixhnq590g.html
+pause
+cls
+goto start
+
+
+
+
+
+
+
+
+
+
+:hello
+cls
+echo hello
 pause
 cls
 goto start
